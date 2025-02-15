98b3e47ad2ccd6c9092f1a37105c3223825a0b42
730-22 <nika14981488@gmail.com>
2025-02-15 19:09:18 +0300

    edited3

--- app/src/main/java/com/example/kobkot_tanks/MainActivity.kt
+++ app/src/main/java/com/example/kobkot_tanks/MainActivity.kt
@@ -8,20 +8,58 @@ import android.view. KeyEvent.KEYCODE_DPAD_DOWN
 import android.view.KeyEvent.KEYCODE_DPAD_LEFT
 import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
 import android.view. KeyEvent.KEYCODE_DPAD_UP
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.FrameLayout
+import androidx.core.view.marginTop
 import com. example. kobkot_tanks.Direction. UP
 import com. example.kobkot_tanks.Direction.DOWN
 import com. example.kobkot_tanks.Direction. LEFT
 import com. example.kobkot_tanks.Direction.RIGHT
 import com.example.kobkot_tanks.databinding.ActivityMainBinding
 
+const val CELL_SIZE = 50
+
+lateinit var binding: ActivityMainBinding
+
+
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private var editMode = false
+    private val gridDrawer by lazy {
+        GridDrawer(context = this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title = "Menu"
+    }
+
+    private fun swichEditMode(){
+        if (editMode) {
+            gridDrawer.removeGrid()
+        }
+        else {
+            gridDrawer.drawGrid()
+        }
+        editMode = !editMode
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_settings -> {
+                swichEditMode()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -38,19 +76,27 @@ class MainActivity : AppCompatActivity() {
         when (direction) {
             UP -> {
                 binding.myTank.rotation = 0F
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin -= 50
+                if (binding.myTank.marginTop > 0) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin -= CELL_SIZE
+                }
             }
             DOWN -> {
-                binding.myTank.rotation = 180F
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += 50
+                if (binding.myTank.marginTop + binding.myTank.height < binding.container.height / (CELL_SIZE * CELL_SIZE)) {
+                    binding.myTank.rotation = 180F
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+                }
             }
             LEFT -> {
-                binding.myTank.rotation = 270F
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= 50
+                if (binding.myTank.marginTop > 0) {
+                    binding.myTank.rotation = 270F
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
+                }
             }
             RIGHT -> {
-                binding.myTank.rotation = 90F
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += 50
+                if (binding.myTank.marginTop + binding.myTank.height < binding.container.height / (CELL_SIZE * CELL_SIZE)) {
+                    binding.myTank.rotation = 90F
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
+                }
             }
         }
 
